14aaf692dee461a6bed8613f4d31ac3f4f9d48e4
l294265421 <294265421@qq.com>
2015-12-01 14:06:08 +0800

    修正了leetcode134的注释

--- src/main/java/leetcode136/LeetCode136.java
+++ src/main/java/leetcode136/LeetCode136.java
@@ -8,7 +8,8 @@ public class LeetCode136 {
     	/**
     	 * 第一次遇到某个元素时，把它放进helper里，第二次
     	 * 遇到它时，就从helper里面把它删除，于是最终helper
-    	 * 里面只会保存出现一次的那个元素
+    	 * 里面只会保存出现一次（实际上应该是奇数次，如果出现
+    	 * 偶数次，这个算法就会出错）的那个元素
     	 */
         Set<Integer> helper = new HashSet<Integer>();
         for (int num : nums) {
