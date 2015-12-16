39a6688cb34b0b4b1f70c432b548f93046b75319
李云聪 <294265421@qq.com>
2015-12-17 00:27:41 +0800

    leetcode23，打算采用堆排序实现算法

--- src/main/java/leetcode23/LeetCode232.java
+++ src/main/java/leetcode23/LeetCode232.java
@@ -34,6 +34,65 @@ public class LeetCode232 {
 		
 	}
 	
+	/**
+	 * 保持堆得性质
+	 * @param lists 保存堆的数组
+	 * @param i 堆中需要下降的元素
+	 * @param heapLength 堆的长度
+	 */
+	public void minHeapify(ListNode[] lists, int i, int heapLength) {
+		int l = left(i);
+		int r = right(i);
+		int minimum = -1;
+		if (l < heapLength && lists[l].val < lists[i].val) {
+			minimum = l;
+		} else {
+			minimum = i;
+		}
+		
+		if (r < heapLength && lists[r].val < lists[minimum].val) {
+			minimum = r;
+		}
+		if (i != minimum) {
+			ListNode temp = lists[i];
+			lists[i] = lists[minimum];
+			lists[minimum] = temp;
+			minHeapify(lists, minimum, heapLength);
+		}
+	}
+	
+	/**
+	 * 建堆
+	 * @param lists 保存堆的数组
+	 * @param heapLength 堆的长度
+	 */
+	public void buildMinHeap(ListNode[] lists, int heapLength) {
+		//拥有子节点的最大下标
+		int start = parent(heapLength - 1);
+		for(int i = start; i >= 0; i--) {
+			minHeapify(lists, i, heapLength);
+		}
+	}
+	
+	private int left(int i) {
+		return i * 2 + 1;
+	}
+	
+	private int right(int i) {
+		return i * 2 + 2;
+	}
+	
+	private int parent(int i) {
+		if (i == 0) {
+			return 0;
+		}
+		if (i % 2 == 0) {
+			return i / 2 - 1;
+		} else {
+			return i / 2;
+		}
+	}
+	
 	public static void main(String[] args) {
 		LeetCode232 leetCode23 = new LeetCode232();
 		ListNode listNode1 = new ListNode(0);
