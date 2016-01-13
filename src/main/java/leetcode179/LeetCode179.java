f40d90718ebe30c05c9bbe73875cd077f89d0d15
liyuncong <liyuncong@liyuncong-PC>
2016-01-13 20:31:05 +0800

    leetcode179

---
+++ src/main/java/leetcode179/LeetCode179.java
@@ -0,0 +1,19 @@
+package leetcode179;
+
+public class LeetCode179 {
+    public String largestNumber(int[] nums) {
+        
+    }
+    
+    public static void main(String[] args) {
+		
+	}
+    
+    /**
+     * 任意组合，找出最大的；
+     * 组合方式，元素下标的排列组合；
+     */
+    public void method1(int[] nums) {
+		
+	}
+}
