8b987d651c0cdef1098e0ae1e5b31d196001c4d4
liyuncong <liyuncong@172.22.21.35>
2016-04-14 18:55:49 +0800

    LeetCode135

---
+++ src/main/java/leetcode135/LeetCode135.java
@@ -0,0 +1,57 @@
+package leetcode135;
+
+public class LeetCode135 {
+	public boolean isSelfCrossing (int[] x) 
+	{ 
+
+	    if (x.length < 4)
+	        return false;
+
+	    for (int i = 3 ; i < x.length ; i++) {
+	        if (isCrossing_2(x, i)) {
+	            return true;
+	        }
+
+	        if (i > 4 && isCrossing_4(x, i))
+	            return true;
+
+	        if (i == 4 && x[i-1] == x[i-3] && x[i] >= x[i-2] - x[i-4])
+	            return true;
+	    }
+
+	    return false;
+	}
+	
+	/**
+	 * Check crossing for last 2 movements along same direction；
+	 * 比较上界
+	 * @param x
+	 * @param i
+	 * @return
+	 */
+	public boolean isCrossing_2(int[] x, int i) {
+	    if (x[i-1] <= x[i-3] && x[i] >= x[i-2])
+	        return true;
+
+	    return false;
+	}
+	
+	/**
+	 * Check crossing for last 4 movements along same direction；
+	 * 比较下界
+	 * @param x
+	 * @param i
+	 * @return
+	 */
+	public boolean isCrossing_4(int[] x, int i) {
+	    if (x[i-1] <= x[i-3] && x[i-1] >= x[i-3] - x[i-5] && x[i-2] >= x[i-4] && x[i] >=  x[i-2] - x[i-4])
+	        return true;
+
+	    return false;
+	}
+	
+	public static void main(String[] args) {
+		int[] x = new int[] {10, 5, 8, 3, 11};
+		System.out.println(new LeetCode135().isSelfCrossing(x));
+	}
+}
