1225c19eb984c9b91f1dc89fac15c1a9e89e543f
liyuncong <liyuncong@172.22.21.35>
2016-04-22 12:45:30 +0800

    中根遍历二叉树迭代实现。leetcode94

--- src/main/java/leetcode94/LeetCode941.java
+++ src/main/java/leetcode94/LeetCode941.java
@@ -3,21 +3,68 @@ package leetcode94;
 import global.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class LeetCode941 {
 	public List<Integer> inorderTraversal(TreeNode root) {
 		List<Integer> resultList = new ArrayList<Integer>();
-		inorderTraversal(root, resultList);
+		if (root == null) {
+			return resultList;
+		}
+		LinkedList<TreeNodeWithFlag> stack = new LinkedList<>();
+		stack.push(new TreeNodeWithFlag(root, false));
+		while (!stack.isEmpty()) {
+			TreeNodeWithFlag temp = stack.peek();
+			if (temp.getTreeNode().left != null && !temp.flag) {
+				stack.push(new TreeNodeWithFlag(temp.getTreeNode().left, false));
+				// 当temp再次处于栈顶时，它的左子树必定已经被访问过了
+				temp.setFlag(true);
+			} else {
+				resultList.add(temp.getTreeNode().val);
+				stack.pop();
+				if (temp.getTreeNode().right != null) {
+					stack.push(new TreeNodeWithFlag(temp.getTreeNode().right, false));
+				}
+			}
+		}
+		
 		return resultList;
 	}
+	
+	private static class TreeNodeWithFlag {
+		private TreeNode treeNode;
+		// 表示该节点的左子树是否被访问过
+		private boolean flag;
 
-	public void inorderTraversal(TreeNode root, List<Integer> resultList) {
-		if (root != null) {
-			inorderTraversal(root.left, resultList);
-			resultList.add(root.val);
-			inorderTraversal(root.right, resultList);
+		private TreeNodeWithFlag(TreeNode treeNode, boolean flag) {
+			super();
+			this.treeNode = treeNode;
+			this.flag = flag;
 		}
+
+		public TreeNode getTreeNode() {
+			return treeNode;
+		}
+
+		public void setTreeNode(TreeNode treeNode) {
+			this.treeNode = treeNode;
+		}
+
+		public boolean isFlag() {
+			return flag;
+		}
+
+		public void setFlag(boolean flag) {
+			this.flag = flag;
+		}
+
+		@Override
+		public String toString() {
+			return "TreeNodeWithFlag [treeNode=" + treeNode + ", flag=" + flag
+					+ "]";
+		}
+
 	}
 	
 	public static void main(String[] args) {
