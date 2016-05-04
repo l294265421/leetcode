184bef3326ae746ac926895a7e860ab7f8735bf2
liyuncong <liyuncong@172.22.21.35>
2016-05-04 13:08:47 +0800

    leetcode146

--- src/main/java/leetcode146/LRUCache.java
+++ src/main/java/leetcode146/LRUCache.java
@@ -1,46 +1,62 @@
 package leetcode146;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * 算法讲解： http://flychao88.iteye.com/blog/1977653
  * 更具体的讲，该算法是淘汰最久没有使用的元素
  * @author liyuncong
- * Time Limit Exceeded
  */
 public class LRUCache {
 	private LinkedList<Pair> cache;
+	// 存的内容与cache一致，用于辅助cache，提供速度，实际上就是用空间换时间
+	private Map<Integer, Pair> helper;
 	private int capacity;
     public LRUCache(int capacity) {
         cache = new LinkedList<>();
+        helper = new HashMap<Integer, LRUCache.Pair>();
         this.capacity = capacity;
     }
     
+    /**
+     * 如果存在，找到key对应的值，并把这个键值对放到队列的开头；
+     * HashMap加链表可以在O(1)内完成(LinkedList删除一个元素的时间复杂度实际上是O(n))
+     * @param key
+     * @return
+     */
     public int get(int key) {
-    	int size = cache.size();
-        for(int i = 0; i < size; i++) {
-        	Pair pair = cache.get(i);
-        	if (pair.getKey() == key) {
-        		cache.remove(i);
-        		cache.addFirst(pair);
-				return pair.getValue();
-			}
-        }
+        Pair pair = helper.get(key);
+        if (pair != null) {
+        	cache.remove(pair);
+    		cache.addFirst(pair);
+    		return pair.getValue();
+		}
+        
         return -1;
     }
     
+    /**
+     * 当缓存中已经存在该key，就修改对应的值，如果没有，就插入；
+     * 插入键值对，当缓存满的时候，需要先删除缓存中最久没有使用的元素，然后再插入；
+     * HashMap加链表可以在O(1)内完成
+     * @param key
+     * @param value
+     */
     public void set(int key, int value) {
-        for (Pair pair : cache) {
-			if (pair.getKey() == key) {
-				pair.setValue(value);
-				return;
-			}
+        Pair oldElement = helper.get(key);
+        if (oldElement != null) {
+        	oldElement.setValue(value);
+			return;
 		}
         Pair newElement = new Pair(key, value);
         if (cache.size() == capacity) {
-			cache.removeLast();
+			Pair lastElement = cache.removeLast();
+			helper.remove(lastElement.getKey());
 		}
         cache.addFirst(newElement);
+        helper.put(key, newElement);
     }
     
     private static class Pair {
