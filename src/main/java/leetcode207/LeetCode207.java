363b946e0c2fa22d0f6e75aa63cc14f49d70cef1
liyuncong <liyuncong@172.22.21.85>
2016-06-16 17:52:30 +0800

    leetcode207

--- src/main/java/leetcode207/LeetCode207.java
+++ src/main/java/leetcode207/LeetCode207.java
@@ -3,6 +3,30 @@ package leetcode207;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+For example:
+
+2, [[1,0]]
+
+There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+
+2, [[1,0],[0,1]]
+
+There are a total of 2 courses to take. To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+Note:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. 
+Read more about how a graph is represented {@link https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs}}. 
+ * @author liyuncong
+ *
+ */
 public class LeetCode207 {
 	static final int UNVISITED = 0;
 	static final int NOW_VISITING = 1;
@@ -29,12 +53,13 @@ public class LeetCode207 {
 	}
 
 	/**
+	 * 判断从邻接表（图的表示）的一个节点出发，是否存在环
 	 * 思路：深度优先访问图，如果一个节点还没被访问结束，而且从该节点的后继元素出发又回到了
 	 * 这个节点，就说明图中有环。
-	 * @param graph
-	 * @param stat
-	 * @param node
-	 * @return
+	 * @param graph 图的邻接表表示
+	 * @param stat 图中各节点的状态
+	 * @param node 节点
+	 * @return 从节点node出发，存在环返回true，否则返回false
 	 */
 	private boolean hasCycle(List<Integer>[] graph, int[] stat, int node) {
 		if (stat[node] == VISITED)
@@ -47,7 +72,6 @@ public class LeetCode207 {
 
 		for (int sub : graph[node]) {
 			if (hasCycle(graph, stat, sub)) {
-				stat[node] = VISITED;
 				return true;
 			}
 
