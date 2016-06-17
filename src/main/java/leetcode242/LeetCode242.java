2ffda3ee8ffcf0b109f5641197b0bf88156d83f7
l294265421 <294265421@qq.com>
2016-06-17 18:01:27 +0800

    LeetCode242

---
+++ src/main/java/leetcode242/LeetCode242.java
@@ -0,0 +1,29 @@
+package leetcode242;
+
+/**
+ * 
+Given two strings s and t, write a function to determine if t is an anagram of s.
+
+For example,
+s = "anagram", t = "nagaram", return true.
+s = "rat", t = "car", return false.
+
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+ * @author liyuncong
+ *
+ */
+public class LeetCode242 {
+    public boolean isAnagram(String s, String t) {
+        
+    }
+    
+    private interface judgeStragedy {
+    	public boolean judge(String s, String t);
+    }
+    
+    private static class Length
+}
