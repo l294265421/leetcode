b4a941be844c6576dbf0ece10bcac6964a8effae
yuncong_li <yuncong_li@kingdee.com>
2017-08-07 14:28:09 +0800

    leetcode264

--- src/main/java/leetcode264/UglyNumberII.java
+++ src/main/java/leetcode264/UglyNumberII.java
@@ -4,7 +4,8 @@ package leetcode264;
  * 
 Write a program to find the n-th ugly number.
 
-Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 1, 2, 
+3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
 
 Note that 1 is typically treated as an ugly number. 
  * @author liyuncong
@@ -12,7 +13,7 @@ Note that 1 is typically treated as an ugly number.
  */
 public class UglyNumberII {
     public int nthUglyNumber(int n) {
-    	return strategy1(n);
+    	return strategy2(n);
     }
     
     public int strategy1(int n) {
@@ -55,23 +56,53 @@ public class UglyNumberII {
 		}
     }
     
-    public int strategy(int n) {
-        int[] a = new int[n];
-        a[0] = 1;
-        int p2 = 0, p3 = 0, p5 = 0;
-        for (int i = 1; i < n; i++) {
-            while (2 * a[p2] <= a[i - 1]) {
-                p2++;
-            }
-            while (3 * a[p3] <= a[i - 1]) {
-                p3++;
-            }
-            while (5 * a[p5] <= a[i - 1]) {
-                p5++;
-            }
-            a[i] = Math.min(2 * a[p2], Math.min(3 * a[p3], 5 * a[p5]));
-        }
-        return a[n - 1];
+    /**
+     * The ugly-number sequence is 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, …
+because every number can only be divided by 2, 3, 5, one way to look at the sequence is to 
+split the sequence to three groups as below:
+
+(0) 1×1, 2×1, 3×1, 4×1, 5×1, …
+(1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+(2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+(3) 1×5, 2×5, 3×5, 4×5, 5×5, …
+
+We can find that every subsequence is the ugly-sequence itself (1, 2, 3, 4, 5, …)(并不是1, 2, 3,
+4, 5, 6, 7, 而是1, 2, 3, 4, 5, 6, 8,) multiply 2, 3, 5.
+
+Then we use similar merge method as merge sort, to get every ugly number from the three subsequence.
+（怎么证明序列(1) (2) (3)合并起来包含所有ugly number？）
+
+Every step we choose the smallest one, and move one step after,including nums with same value.
+
+Thanks for this author about this brilliant idea. Here is my java solution
+     * @param n
+     * @return
+     */
+    public int strategy2(int n) {
+    	 int[] ugly = new int[n];
+         ugly[0] = 1;
+         // 2 3 5 应该乘ugly number序列中的哪个位置的元素了，用于获得分解后三个序列中相应位置的值
+         int index2 = 0;
+         int index3 = 0; 
+         int index5 = 0;
+         int factor2 = 2;
+         int factor3 = 3;
+         int factor5 = 5;
+         for(int i=1; i<n; i++){
+             int min = Math.min(Math.min(factor2,factor3),factor5);
+             ugly[i] = min;
+             // 同一个最小值只会取一次
+             if(factor2 == min) {
+            	 factor2 = 2*ugly[++index2];
+             }
+             if(factor3 == min) {
+            	 factor3 = 3*ugly[++index3];
+             }
+             if(factor5 == min) {
+            	 factor5 = 5*ugly[++index5];
+             }
+         }
+         return ugly[n-1];
 	}
     
     public static void main(String[] args) {
